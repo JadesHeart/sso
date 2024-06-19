2dcbb83f14df5aeabe4474571bf192df423ad048
JadesHeart <jadesheartwork@gmail.com>
2024-06-19 06:37:43 +0400

    добавлена обёртка для миграций

--- cmd/migrator/main.go
+++ cmd/migrator/main.go
@@ -1,1 +1,57 @@
-package migrator
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	var storageURL, migrationsPath, migrationsTable string
+
+	flag.StringVar(&storageURL, "storage-url", "", "PostgreSQL connection URL")
+	flag.StringVar(&migrationsPath, "migrations-path", "", "Path to migrations")
+	flag.StringVar(&migrationsTable, "migrations-table", "migrations", "Name of migrations table")
+	flag.Parse()
+
+	if storageURL == "" {
+		panic("storage-url is required")
+	}
+	if migrationsPath == "" {
+		panic("migrations-path is required")
+	}
+
+	m, err := migrate.New(
+		"file://"+migrationsPath,
+		storageURL,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("no migrations to apply")
+			return
+		}
+		panic(err)
+	}
+
+	fmt.Println("migrations applied")
+}
+
+type Log struct {
+	verbose bool
+}
+
+func (l *Log) Printf(format string, v ...interface{}) {
+	fmt.Printf(format, v...)
+}
+
+func (l *Log) Verbose() bool {
+	return false
+}
